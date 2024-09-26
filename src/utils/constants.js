451c9b02d74f6ddb0d4542d3c381f46b05a63ac4
Shekhar Sajwan <shekharsajwan97@gmail.com>
2024-09-26 17:36:55 +0530

    constant .env file created

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,9 @@
 const API_KEY = "AIzaSyC2Rvn6LU0c3lFxLe-nklVFOyHgIe9jH8Q";
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
-  API_KEY;
+  process.env.REACT_APP_YOUTUBE_VIDEOS_API + API_KEY;
 
-export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt";
+export const YOUTUBE_SEARCH_API = process.env.REACT_APP_YOUTUBE_SEARCH_API;
 
 export const YOUTUBE_SEARCH_SUGGESTION_API =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&regionCode=IN&key=" +
-  API_KEY;
+  process.env.REACT_APP_YOUTUBE_SEARCH_SUGGESTION_API + API_KEY;
